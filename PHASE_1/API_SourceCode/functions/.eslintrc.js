7922f63f56222e06c8aa3428d530c10b58d86fd4
Hayes Choy <hayes.choy@gmail.com>
2021-03-11 17:49:45 +1100

    error handling done

---
+++ PHASE_1/API_SourceCode/functions/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "google",
+    ],
+    rules: {
+        "quotes": ["error", "double"],
+        "indent": ["error", 4],
+        "max-len": "off",
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+};
