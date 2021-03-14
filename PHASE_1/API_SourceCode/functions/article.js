40e6556f76e4b4b1e641fd1936be0b5eace2b5cd
Hayes Choy <hayeselnut2127@gmail.com>
2021-03-15 00:26:24 +1100

    separated into files

---
+++ PHASE_1/API_SourceCode/functions/article.js
@@ -0,0 +1,16 @@
+/* eslint-disable guard-for-in */
+const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+const response = require("./responses");
+
+admin.initializeApp();
+const db = admin.firestore();
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    res.status(200).send(response.success({"dummy": "article"}, "article"));
+});
+
+exports.app = functions.https.onRequest(app);
