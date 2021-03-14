d0e749080d9b91fb97bda02e3e6746b9d755c709
LeilaKatY <leilakatyuan@gmail.com>
2021-03-14 14:56:57 +1100

    fixed headline function

--- PHASE_1/API_SourceCode/scraper.py
+++ PHASE_1/API_SourceCode/scraper.py
@@ -50,10 +50,24 @@ def get_USAndTravel(url, link_list):
 def get_headline(url):
     page_soup = get_page_html(url)
     container = page_soup.find("title")
-    if container == None:
-        container = page_soup.find("h1", {"id": "content"})
-    
+    if container != None:
+        container = re.sub('<[^>]+>', '', str(container))
+        container = re.sub(r'\s+\-.*|\s+\|.*',"", container)
+    else:
+        container = "unknown"
     return container
+
+def get_publish_date(url):
+    #page = requests.get("https://www2c.cdc.gov/podcasts/feed.asp?feedid=513&format=json")
+    
+    if requests.get(url) in l1:
+        return t1[l1.index(url)] 
+
+    # if get_page_html(url) in l2:
+    #     return t2[l2.index(get_page_html(url))]
+
+    return "unknown"
+
 
 
 link_list = []
@@ -62,18 +76,52 @@ get_USAndTravel("https://www.cdc.gov/outbreaks/", link_list)
 #get_foodSafety("https://www.cdc.gov/outbreaks/", link_list)
 # print(link_list)
 # print(len(link_list))
+
+url1 = "https://www2c.cdc.gov/podcasts/feed.asp?feedid=513&format=json"
+
+r1 = requests.get(url1)
+
+l1 = []
+t1 = []
+json_data = r1.json()
+count = 0
+for item in json_data['entries']:
+    l1.append(requests.get(item['link']))
+    t1.append(item['pubdate'])
+
+
+# url2 = "https://www2c.cdc.gov/podcasts/feed.asp?feedid=66&format=json"
+
+# r2 = requests.get(url2)
+
+# l2 = []
+# t2 = []
+# json_data = r2.json()
+# count = 0
+# for item in json_data['entries']:
+#     l2.append(get_page_html(item['link']))
+#     t2.append(item['pubdate'])
+
+
+
+
 all_articles = {}
 count = 0
 for url in link_list:
+    #get_publish_date(url)
     article = {}
     report = {}
     article['url'] = url
-    article['date_of_publication'] = url
+    article['date_of_publication'] = get_publish_date(url)
     article['headline'] = get_headline(url)
-    article['maintext'] = url
+    article['maintext'] = "blah"
     article['report'] = report
     all_articles['article'+ str(count)] = article
     count += 1
     print(article)
 # with open('mydata.json', 'w') as f:
 #     json.dump(all_articles, f)
+
+
+#https://www2c.cdc.gov/podcasts/feed.asp?feedid=513&format=json
+
