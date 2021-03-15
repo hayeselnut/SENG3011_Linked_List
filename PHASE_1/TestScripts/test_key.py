b4c98aac38abacc592e882d55c0614a7525af1e3
Xiyang Shi <xiyang.shi@student.unsw.edu.au>
2021-03-15 17:53:49 +0800

    add sometest

--- PHASE_1/TestScripts/test_key.py
+++ PHASE_1/TestScripts/test_key.py
@@ -32,8 +32,50 @@ def test_Bitcoin():
     assert(not req.ok)
     assert(req.status_code == 400)
 
+def test_Haemorrhagic_Fever():
+    PARAMS = {
+        "start_date": "2020-01-01T08:00:00",
+        "end_date": "2021-03-01T08:00:00",
+        "location": "China",
+        "key_term": "Haemorrhagic Fever"
+    }
 
+    req = requests.get(url = URL, params = PARAMS)
+
+    assert(req.request.method == "GET")
+    assert(req.ok)
+    assert(req.status_code == 200)
+
+def test_unknown():
+    PARAMS = {
+        "start_date": "2020-01-01T08:00:00",
+        "end_date": "2021-03-01T08:00:00",
+        "location": "China",
+        "key_term": "Haemorrhagic Fever,unknown"
+    }
+
+    req = requests.get(url = URL, params = PARAMS)
+
+    assert(req.request.method == "GET")
+    assert(req.ok)
+    assert(req.status_code == 200)
+
+def test_other():
+    PARAMS = {
+        "start_date": "2020-01-01T08:00:00",
+        "end_date": "2021-03-01T08:00:00",
+        "location": "China",
+        "key_term": "Influenza-like illness,other"
+    }
+
+    req = requests.get(url = URL, params = PARAMS)
+
+    assert(req.request.method == "GET")
+    assert(req.ok)
+    assert(req.status_code == 200)
 
 
 test_Covid()
 test_Bitcoin()
+test_Haemorrhagic_Fever()
+test_other()
