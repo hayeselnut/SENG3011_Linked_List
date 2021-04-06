f6a0e58c5d75b41b8886e3f84926be0c4e0d84d2
Thomas Sinn <z5213546@unsw.edu.au>
2021-04-06 16:15:20 +1000

    Started React App

---
+++ PHASE_2/Application_SourceCode/app/home/home.js
@@ -0,0 +1,13 @@
+
+
+const home = () => {
+    <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" type="text"/>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" type="text"/>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" type="date"/>
+    </form>
+
+    return (
+        <></>
+    )
+}
---
+++ PHASE_2/Application_SourceCode/app/src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/map">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/map">
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Map() {
+  return <h2>Map</h2>;
+}
+
+export default App;
