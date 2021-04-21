2ac15bdc6803cb295e43a16089005e58d3135fac
Xiyang Shi <xiyang.shi@student.unsw.edu.au>
2021-04-21 23:29:37 +0800

    add

--- PHASE_2/Application_SourceCode/app/src/map/getcoord.js
+++ PHASE_2/Application_SourceCode/app/src/map/getcoord.js
@@ -1,15 +1,25 @@
 import{Coords} from './mapdata'
+import{USCoords} from './USA'
 
 export function getcoord(state,city) {
     // Result is a list of event_date, url and headline
     const value = [];
 
-    if(city == ''){
+    if(city == 'NONE'){
 
-        for (var i  in Coords){
-            if (i.properties.state == state){
-                for(var x in i.coordinates){
-                    value.append({lng:x[0],lat:x[1]});
+       // console.log(Coords);
+        for (var i  in USCoords){
+            //console.log(Coords[i]);
+            var ii = USCoords[i];
+          //  console.log(ii.properties.city,city,ii.properties.state , state);
+            if (ii.properties.state == state){
+                
+                for(var x in ii.coordinates){
+                    var xx = ii.coordinates[x];
+                   // console.log(xx);
+                    var temp = {lng:xx[0],lat : xx[1]}
+                  //  console.log(temp);
+                    value.push(temp);
                 }
             }
         }
