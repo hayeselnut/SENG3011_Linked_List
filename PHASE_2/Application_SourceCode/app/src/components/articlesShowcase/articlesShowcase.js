ccb043de88eecf8bb69aa1a2dd3deeccf61682bc
hchoy <hchoy@atlassian.com>
2021-04-29 22:10:58 +1000

    styled toolbar

--- PHASE_2/Application_SourceCode/app/src/components/articlesShowcase/articlesShowcase.js
+++ PHASE_2/Application_SourceCode/app/src/components/articlesShowcase/articlesShowcase.js
@@ -9,7 +9,7 @@ const ArticlesShowcase = (props) => {
 
   return (
     <div style={{ marginTop: "4em" }}>
-      <Typography component="h2" variant="h5">Outbreak information near {province}</Typography>
+      <Typography style={{marginLeft: '1em', marginRight: '1em'}} component="h2" variant="h5">Outbreak information near {province}</Typography>
       <div style={{ height: "50vh", overflow: "scroll"}}>
         {articlesLoading
           ? <CircularProgress style={{margin: "1em"}}/>
--- PHASE_2/Application_SourceCode/app/src/components/toolbar/epiwatchToolbar.js
+++ PHASE_2/Application_SourceCode/app/src/components/toolbar/epiwatchToolbar.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Typography, Toolbar, IconButton, Link, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core'
-import HomeIcon from '@material-ui/icons/HomeRounded';
+import { Typography, Toolbar, IconButton, Link, FormControl, InputLabel, MenuItem, Select, AppBar } from '@material-ui/core'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import { makeStyles } from '@material-ui/core/styles';
 import SupportedCountries from "../../assets/SupportedCountries.json"
@@ -23,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
   formStateControl: {
     margin: theme.spacing(1),
     width: 200,
+  },
+  toolbar: {
+    ...theme.mixins.toolbar,
   }
 }));
 
@@ -31,47 +33,45 @@ const EpiWatchToolBar = (props) => {
   const { pageName, country, setCountry, province, setProvince, recordedCases, predictedCases } = props;
 
   return (
-    <Toolbar>
-      <Typography variant="h6" className={classes.title}>{pageName}</Typography>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>{pageName}</Typography>
 
-      <FormControl variant="outlined" className={classes.formCountryControl}>
-        <InputLabel id="country-label">Country</InputLabel>
-        <Select
-          labelId="country-label"
-          id="country-select"
-          label="Country"
-          value={country}
-          onChange={(e) => {setCountry(e.target.value); setProvince(SupportedCountries[e.target.value].Provinces[0])}}
-        >
-          {Object.keys(SupportedCountries).map((key, index) => (
-            <MenuItem key={index} value={SupportedCountries[key].Slug}>{SupportedCountries[key].Country}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <FormControl variant="outlined" className={classes.formCountryControl}>
+          <Select
+            id="country-select"
+            value={country}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => {setCountry(e.target.value); setProvince(SupportedCountries[e.target.value].Provinces[0])}}
+          >
+            {Object.keys(SupportedCountries).map((key, index) => (
+              <MenuItem key={index} value={SupportedCountries[key].Slug}>{SupportedCountries[key].Country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <FormControl variant="outlined" className={classes.formStateControl}>
-        <InputLabel id="state-label">State</InputLabel>
-        <Select
-          labelId="state-label"
-          id="state-select"
-          label="State"
-          value={province}
-          onChange={(e) => setProvince(e.target.value)}
-        >
-          {SupportedCountries[country].Provinces.map((province, index) => (
-            <MenuItem key={index} value={province}>{province}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+        <FormControl variant="outlined" className={classes.formStateControl}>
+          <Select
+            id="state-select"
+            value={province}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setProvince(e.target.value)}
+          >
+            {SupportedCountries[country].Provinces.map((province, index) => (
+              <MenuItem key={index} value={province}>{province}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <CasesChartModal state={province} recorded={recordedCases[province]} predicted={predictedCases[province]} />
+        <CasesChartModal state={province} recorded={recordedCases[province]} predicted={predictedCases[province]} />
 
-      <Link href="https://github.com/hayeselnut/SENG3011_Linked_List" color="inherit" target="_blank">
-        <IconButton className={classes.menuButton} color="inherit" aria-label="github">
-          <GitHubIcon />
-        </IconButton>
-      </Link>
-    </Toolbar>
+        <Link href="https://github.com/hayeselnut/SENG3011_Linked_List" color="inherit" target="_blank">
+          <IconButton className={classes.menuButton} color="inherit" aria-label="github">
+            <GitHubIcon />
+          </IconButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 }
 
